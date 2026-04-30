4cab7a558805fd5c768f45545231079ab80b97cd
Isabela Davolio Wagemaker <isabelaw@cpqd.com.br>
2026-04-30 09:48:56 -0300

    Tela de carregamento funcionando na introdução

--- Assets/Game/Scripts/LoadingScreenController.cs
+++ Assets/Game/Scripts/LoadingScreenController.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class LoadingScreenController : MonoBehaviour
 {
     // Este método será chamado pelo evento da animação
     public void FinalizarCarregamento()
     {
+        Image img = transform.parent.GetComponent<Image>();
+
         // Desliga o objeto pai (o painel de loading)
         transform.parent.gameObject.SetActive(false);
+        // desativa o raycast target do pai
+       if (img != null)
+        {
+            img.raycastTarget = false;
+        }
     }
 }
 
